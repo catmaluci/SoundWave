bf3555bd7d9b9b5275dae1be909284ff7e6eb0ee
Mariuxi <catmaluci@hotmail.com>
2025-07-16 16:48:42 +0200

    improving features of the layout

--- src/components/Mains/Main_app/Main.jsx
+++ src/components/Mains/Main_app/Main.jsx
@@ -17,7 +17,7 @@ function Main() {
             </div>
             <div className="divText">
                 <h1>Feel The Music</h1>
-                <h6>Stream over 20 thousand songs with one click</h6>
+                <p>Stream over 20 thousand songs with one click</p>
 
                 <Button type="button" navigateTo="/Join" />
             </div>
